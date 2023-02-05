76857a95a4aa8a514438ac60ee1b28442bc01b96
ecclesias <77536370+zeviel@users.noreply.github.com>
2023-02-05 19:20:00 +0800

    Create 1secmail.sh

---
+++ src/1secmail.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+api="https://www.1secmail.com/api/v1"
+
+function generate_email() {
+	curl --request GET \
+		--url "$api/?action=genRandomMailbox&count=$1" \
+		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--header "accept: application/json"	\
+		--header "content-type: application/json"
+}
+
+function get_domains() {
+	curl --request GET \
+		--url "$api/?action=getDomainList" \
+		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--header "accept: application/json"	\
+		--header "content-type: application/json"
+}
+
+function get_messages() {
+	curl --request GET \
+		--url "$api/?action=getMessages&login=$1&domain=$2" \
+		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--header "accept: application/json"	\
+		--header "content-type: application/json"
+}
+
+function read_message() {
+	curl --request GET \
+		--url "$api/?action=readMessage&login=$1&domain=$2&id=$3" \
+		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--header "accept: application/json"	\
+		--header "content-type: application/json"
+}
